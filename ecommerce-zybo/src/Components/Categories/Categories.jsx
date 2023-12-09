f3deea9fa649710964df8439388ffad5de0e5d9c
aswanthnarayan <aswanthnarayanan@gmail.com>
2023-12-09 08:37:28 +0530

    completed

--- ecommerce-zybo/src/Components/Categories/Categories.jsx
+++ ecommerce-zybo/src/Components/Categories/Categories.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Categories.css';
 import SingleCategorie from './SingleCategorie';
 import cat1Img from '../../assets/Monstera deliciosa plant in a pot.png'
-// import cat1Img from '../../assets/Monstera deliciosa plant in a pot.png'
-// import cat1Img from '../../assets/Monstera deliciosa plant in a pot.png'
+import cat2Img from '../../assets/Empty terracotta clay plant pot.png'
+ import cat3Img from '../../assets/Potted house plant isolated in white background.png'
 
 
 function Categories() {
@@ -13,8 +13,8 @@ function Categories() {
             <p>Categories</p>
           <div className="content-container">
             <SingleCategorie imgSrc={cat1Img} nameOfImg='Plants'/>
-            <SingleCategorie imgSrc={cat1Img} nameOfImg='Plants'/>
-            <SingleCategorie imgSrc={cat1Img} nameOfImg='Plants'/> 
+            <SingleCategorie imgSrc={cat2Img} nameOfImg='Pots'/>
+            <SingleCategorie imgSrc={cat3Img} nameOfImg='Combos'/> 
             <div className="button-container">
               <button>See more</button>
             </div>
--- ecommerce-zybo/src/Components/SubCategories/SubCategories.jsx
+++ ecommerce-zybo/src/Components/SubCategories/SubCategories.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './SubCategories.css'
-import cat1Img from '../../assets/Monstera deliciosa plant in a pot.png'
+import subcat1Img from '../../assets/AGLAONEMA PINK 1.png'
+import subcat2Img from '../../assets/Sansevieria Fernwood Mikado 1.png'
+import subcat3Img from '../../assets/GOLDEN KING MARBLE 1.png'
+
 import SingleSubCategories from './SingleSubCategories'
 
 function SubCategories() {
@@ -9,9 +12,9 @@ function SubCategories() {
         <div className="main-SubCategories">
             <p>Sub Categories</p>
             <div className="content-container">
-                <SingleSubCategories imgSrc={cat1Img} nameOfImg='Plants'/>
-                <SingleSubCategories imgSrc={cat1Img} nameOfImg='Plants'/>
-                <SingleSubCategories imgSrc={cat1Img} nameOfImg='Plants'/>
+                <SingleSubCategories imgSrc={subcat1Img} nameOfImg='Flowering Plants'/>
+                <SingleSubCategories imgSrc={subcat1Img} nameOfImg='Low-Light Plants'/>
+                <SingleSubCategories imgSrc={subcat3Img} nameOfImg='PlaFlowering Plants'/>
             </div>
         </div>
         <div className="sub-categories"></div>
